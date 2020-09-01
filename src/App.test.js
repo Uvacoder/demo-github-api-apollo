f7cf861c3c43c37066a2f6449642a71a679a6baf
dawneraq <dawneraq@gmail.com>
2020-09-01 10:03:18 -0700

    remove boilerplate

--- src/App.test.js
+++ src/App.test.js
@@ -2,8 +2,8 @@ import React from 'react';
 import { render } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('says hello world', () => {
   const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const helloElement = getByText(/hello world/i);
+  expect(helloElement).toBeInTheDocument();
 });
