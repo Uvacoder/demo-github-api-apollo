54cdc83274f4f0a46e6071330e8be9e6cc6a01a5
dawneraq <dawneraq@gmail.com>
2020-09-03 13:39:07 -0700

    add Pagination component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,9 @@
 import React, { useState, useEffect } from 'react';
 import { gql, useLazyQuery } from '@apollo/client';
 
-// TODO Modify search params for pagination
 const USERS = gql`
-  query GetUsers($userQuery: String!) {
-    search(query: $userQuery, type: USER, first: 10) {
+  query GetUsers($userQuery: String!, $pageLength: Int, $cursor: String) {
+    search(query: $userQuery, type: USER, first: $pageLength, after: $cursor) {
       userCount
       edges {
         node {
@@ -30,31 +29,50 @@ const USERS = gql`
             createdAt
           }
         }
+        cursor
       }
     }
   }
 `;
 
+const PAGE_LENGTH = 10;
+
 const App = () => {
   const [userQuery, setUserQuery] = useState('');
   const [userCount, setUserCount] = useState(0);
   const [usersPage, setUsersPage] = useState([]);
-  const [getUsers, { loading, data }] = useLazyQuery(USERS);
+  const [offset, setOffset] = useState(0);
+  // TODO How to synchronize cursor w/offset for Pagination?
+  // setCursor should be called with fetchMore
+  const [cursor, setCursor] = useState(null);
+  const [getUsers, { loading, data, fetchMore }] = useLazyQuery(USERS);
 
   useEffect(() => {
     if (data) {
       console.log('got API data:', data);
-      setUserCount(data.search.userCount);
-      setUsersPage(
-        data.search.edges.filter((edge) => edge.node.__typename === 'User')
+
+      // TODO This only accounts for when Organization results appear in the first page
+      // What if instead of stripping out Organizations, we showed them too?
+      const onlyUsers = data.search.edges.filter(
+        (edge) => edge.node.__typename === 'User'
       );
+      setUserCount(
+        data.search.userCount - (data.search.edges.length - onlyUsers.length)
+      );
+      setUsersPage(onlyUsers);
     }
   }, [loading, data]);
 
   const handleSubmit = (event) => {
     event.preventDefault();
 
-    getUsers({ variables: { userQuery: userQuery } });
+    getUsers({
+      variables: {
+        userQuery: userQuery,
+        pageLength: PAGE_LENGTH,
+        cursor: cursor,
+      },
+    });
   };
 
   return (
@@ -80,19 +98,18 @@ const App = () => {
         <p>Loading users...</p>
       ) : (
         <>
-          {userCount !== 0 ? (
-            <p>{userCount} users found</p>
-          ) : (
-            <p>No users to show</p>
-          )}
           {usersPage.length ? (
-            <ul>
-              {usersPage.map((user) => (
-                <li key={user.node.id}>
-                  <UserListing {...user.node} />
-                </li>
-              ))}
-            </ul>
+            <>
+              <Pagination {...{ userCount, offset, setOffset }} />
+              <ul>
+                {usersPage.map((user) => (
+                  <li key={user.node.id}>
+                    <UserListing {...user.node} />
+                  </li>
+                ))}
+              </ul>
+              <Pagination {...{ userCount, offset, setOffset }} />
+            </>
           ) : null}
         </>
       )}
@@ -137,5 +154,39 @@ const UserListing = ({
     </ul>
   </a>
 );
+
+const Pagination = ({ userCount, offset, setOffset }) => {
+  const lowerBound = 1 + offset;
+  const upperBound = PAGE_LENGTH + offset;
+  const canClickPrevious = offset > 0;
+  const canClickNext = upperBound < userCount;
+
+  const handleClickPrevious = () => {
+    if (canClickPrevious) setOffset(offset - PAGE_LENGTH);
+  };
+
+  const handleClickNext = () => {
+    if (canClickNext) setOffset(offset + PAGE_LENGTH);
+  };
+
+  return (
+    <>
+      {userCount !== 0 ? (
+        <p>
+          Showing {lowerBound} - {canClickNext ? upperBound : userCount} of{' '}
+          {userCount} user{userCount > 1 ? 's' : null} found
+        </p>
+      ) : (
+        <p>No users to show</p>
+      )}
+      <button onClick={handleClickPrevious} disabled={!canClickPrevious}>
+        Previous
+      </button>
+      <button onClick={handleClickNext} disabled={!canClickNext}>
+        Next
+      </button>
+    </>
+  );
+};
 
 export default App;
